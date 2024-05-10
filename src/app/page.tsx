e0c16d37048706199c919623cdc8bdc82c2b2015
Yao TengXiong <yao_tengxiong@hotmail.com>
2024-05-10 12:04:28 +0800

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { getImages } from "@/utils/cloudinary";
-import { Globe } from "lucide-react";
+import { Home as HomeIcon } from "lucide-react";
 
 async function Home() {
   const images = await getImages();
@@ -9,7 +9,10 @@ async function Home() {
     <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 min-h-screen gap-4 py-24 px-12">
       <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 flex justify-between items-start">
         <div>
-          <p className="text-2xl font-bold tracking-wide">film photos</p>
+          <p className="text-2xl font-bold tracking-wide">film photos (unedited)</p>
+          <p className="text-xs tracking-tight">
+            Olympus OM-10 / Portra 400
+          </p>
           <p className="text-xs tracking-tight">by&nbsp;
             <a
               href="https://github.com/yao-tx"
@@ -21,14 +24,14 @@ async function Home() {
             </a>
           </p>
         </div>
-          <a
-            href="https://yaotx.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-1 hover:opacity-90"
-          >
-            <Globe className="w-5 h-5" />
-          </a>
+        <a
+          href="https://yaotx.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-1 hover:opacity-90"
+        >
+          <HomeIcon className="w-5 h-5" />
+        </a>
       </div>
       {images.map(({ id, src, title, alt, blurDataURL }) => {
         return (
@@ -41,7 +44,7 @@ async function Home() {
             title={title}
             placeholder="blur"
             blurDataURL={blurDataURL}
-            className="w-full brightness-90 hover:brightness-110"
+            className="w-full brightness-90 hover:brightness-110 hover:cursor-zoom-in"
           />
         )
       })}
